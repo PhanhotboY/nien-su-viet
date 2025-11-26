e9ffc5679aa2400418bfb55438169b470328f1ff
PhanhotboY <nguyenduyphan2003@gmail.com>
2025-11-26 16:40:09 +0700

    remove user service

--- apps/user/src/config/envs/default.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  db: {
-    // entities: [`${__dirname}/../../entity/**/*.{js,ts}`],
-    // subscribers: [`${__dirname}/../../subscriber/**/*.{js,ts}`],
-    // migrations: [`${__dirname}/../../migration/**/*.{js,ts}`],
-  },
-};
