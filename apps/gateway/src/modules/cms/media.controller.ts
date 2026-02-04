ea2f05f3d3ef2d747087b422b734cd97a71fe05b
PhanhotboY <nguyenduyphan2003@gmail.com>
2026-02-04 08:00:12 +0700

    fix gateway caching issue

--- apps/gateway/src/modules/cms/media.controller.ts
+++ apps/gateway/src/modules/cms/media.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Delete, Get, Post, Put, Req } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Post,
+  Put,
+  Req,
+} from '@nestjs/common';
 import type { Request } from 'express';
 
 import { HttpProxyService } from '@gateway/common/services/http-proxy.service';
 import { Permissions, Public } from '@gateway/common/decorators';
 import { Throttle } from '@nestjs/throttler';
 import { RATE_LIMIT } from '@gateway/config';
+import { RedisService, type RedisServiceType } from '@phanhotboy/nsv-common';
 
 // ref: apps/cms/internal/media/controller/http/media.router.go
 @Controller('media')
 export class MediaController {
-  constructor(private readonly cmsProxy: HttpProxyService) {}
+  private readonly routePath = '/api/v1/media*';
+  constructor(
+    private readonly cmsProxy: HttpProxyService,
+    @Inject(RedisService) private readonly redis: RedisServiceType,
+  ) {}
 
   @Get()
   @Public()
@@ -20,21 +33,24 @@ export class MediaController {
   @Post()
   @Throttle(RATE_LIMIT.INTERNAL)
   @Permissions({ media: ['create'] })
-  ProxyPostRequest(@Req() req: Request) {
-    return this.cmsProxy.proxyRequest(req);
+  async ProxyPostRequest(@Req() req: Request) {
+    await this.redis.mdel(this.routePath);
+    return await this.cmsProxy.proxyRequest(req);
   }
 
   @Put(':id')
   @Throttle(RATE_LIMIT.INTERNAL)
   @Permissions({ media: ['delete'] })
-  proxyPutRequest(@Req() req: Request) {
-    return this.cmsProxy.proxyRequest(req);
+  async proxyPutRequest(@Req() req: Request) {
+    await this.redis.mdel(this.routePath);
+    return await this.cmsProxy.proxyRequest(req);
   }
 
   @Delete(':id')
   @Throttle(RATE_LIMIT.INTERNAL)
   @Permissions({ media: ['delete'] })
-  proxyDeleteRequest(@Req() req: Request) {
-    return this.cmsProxy.proxyRequest(req);
+  async proxyDeleteRequest(@Req() req: Request) {
+    await this.redis.mdel(this.routePath);
+    return await this.cmsProxy.proxyRequest(req);
   }
 }
